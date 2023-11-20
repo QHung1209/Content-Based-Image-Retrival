f42be2641d78e14b8bbe61bae26b1b18806450b0
unknown <21522117@gm.uit.edu.vn>
2023-11-20 22:48:22 +0700

    erro image 404

--- app.py
+++ app.py
@@ -1,10 +1,42 @@
-from flask import Flask, render_template, request, session
-import os
-import base64
-import cv2
-
+from flask import Flask, render_template, request, session,jsonify
+from keras.preprocessing import image
+from keras.applications.vgg16 import VGG16, preprocess_input
+from keras.models import Model
+import pickle
+import numpy as np
+from cv2 import resize,cvtColor,COLOR_BGR2RGB,imread
+ 
 app = Flask(__name__)
 app.secret_key = "your_secret_key" 
+
+
+vectors = pickle.load(open(".\\vectors.pkl", "rb"))
+
+paths = pickle.load(open(".\\paths.pkl", "rb"))
+
+def get_extract_feature():
+    vgg16_model = VGG16(weights='imagenet')
+    extract_model = Model(inputs=vgg16_model.inputs,
+                          outputs=vgg16_model.get_layer("fc1").output)
+    return extract_model
+
+
+def image_preprocess(img):
+    img = resize(img, (224, 224))
+    img = cvtColor(img, COLOR_BGR2RGB)  
+    x = image.img_to_array(img)
+    x = np.expand_dims(x, axis=0)
+    x = preprocess_input(x)
+    return x
+
+
+def extract_vector(model, img):
+    print("Xy ly: ")
+    img_tensor = image_preprocess(img)
+
+    vector = model.predict(img_tensor)[0]
+    vector = vector / np.linalg.norm(vector)
+    return vector
 
 @app.route("/")
 def home():
@@ -20,28 +52,6 @@ def upload_file():
     file.save(image_path)
     session['image_path'] = image_path
     return render_template("index.html")
-"""
-    data = request.get_json()
-
-    # Access the variables
-    x = data.get('x')
-    y = data.get('y')
-    width = data.get('width')
-    height = data.get('height')
-    image_path = session.get('image_path', None)
-    fileName = data.get('fileName')
-    image_path = ".\\image_upload\\" + fileName
-    print(x, height, fileName)
-
-    img = cv2.imread(image_path)
-
-    # Crop image to specified area using slicing
-    crop_img = img[int(y):int(y+height), int(x):int(x+width)]
-# Show image
-    cv2.imshow("cropped", crop_img)
-    cv2.waitKey(0)
-    return render_template("index.html")
-"""
 
 @app.route('/get_Data', methods=['POST'])
 def get_Data():
@@ -56,15 +66,21 @@ def get_Data():
     fileName = data.get('fileName')
     image_path = ".\\image_upload\\" + fileName
     print(x, height, fileName)
-
-    img = cv2.imread(image_path)
-
+    img = imread(image_path)
     # Crop image 
     crop_img = img[int(y):int(y+height), int(x):int(x+width)]
-# Show image
-    cv2.imshow("cropped", crop_img)
-    cv2.waitKey(0)
-    return render_template("index.html")
+
+    model = get_extract_feature()
+
+    search_vector = extract_vector(model, crop_img)
+
+    distance = [np.linalg.norm(vector - search_vector) for vector in vectors]
+
+    ids = np.argsort(distance)[:5]
+
+    scores = [paths[id] for id in ids]
+
+    return jsonify(results=scores)
 
     
 if __name__ == '__main__':
